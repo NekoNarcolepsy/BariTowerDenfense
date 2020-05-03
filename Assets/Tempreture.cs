d722b136e0d3c3f886a0a714cb4059356534d6ff
LensCheung <lance_ccf@outlook.com>
2020-05-03 20:23:38 +0900

    DEMO和第一关更新

---
+++ Assets/Tempreture.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tempreture : MonoBehaviour
+{
+    public float Temp = 0.0f;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject playerObj = null;
+            if (playerObj == null)
+            {
+                playerObj = GameObject.Find("Player");
+            }
+            playerObj.GetComponent<PlayerMovement>().ChangingTempreture(Temp);
+        }
+    }
+}
