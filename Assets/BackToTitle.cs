1d127ba502c4fac447c024f01f322436844ca5cc
LensCheung <lance_ccf@outlook.com>
2020-12-08 13:06:16 +0900

    通关奖励,继续，回到主页面

---
+++ Assets/BackToTitle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToTitle : MonoBehaviour
+{
+    public string sceneName;
+    public void Title()
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+
+}
